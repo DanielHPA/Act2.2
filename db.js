d9837a8fbb96117e4b8518f84eca2d48a79902d0
DanielHPA <daniel.horacioparedesaguirre@gmail.com>
2025-05-13 14:29:49 -0700

    db.js

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost', // Fallback a localhost si no hay variable de entorno
-    user: process.env.DB_USER || 'root',      // Fallback a root si no hay variable de entorno
-    password: process.env.DB_PASSWORD || '',  // Fallback a contraseña vacía si no hay variable de entorno
-    database: process.env.DB_NAME || 'test',  // Fallback a base de datos test si no hay variable de entorno
+    host: process.env.DB_HOST || 'localhost', 
+    user: process.env.DB_USER || 'root',      
+    password: process.env.DB_PASSWORD || '',  
+    database: process.env.DB_NAME || 'test',  
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
